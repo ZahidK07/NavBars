8c6748c66943caa45a6d5fc3ac8bda57f04a2d2a
ZahidK07 <zaksfit07@gmail.com>
2025-06-14 12:25:30 +0530

    Adding Responsive Navbar folder

---
+++ ResponsiveNavbar/script.js
@@ -0,0 +1,12 @@
+
+// To Slide and open the navigation bar
+function showSideBar(){
+   const sideBar = document.querySelector(".sidebar");
+   sideBar.style.display = "flex"; 
+}
+
+// To hide the Slided navigation bar
+function hideSideBar() {
+    const sideBar = document.querySelector(".sidebar");
+    sideBar.style.display = "none"; 
+}
